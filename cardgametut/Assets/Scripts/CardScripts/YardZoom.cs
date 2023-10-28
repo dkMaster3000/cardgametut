032a2137d1bca42704821d2baa48d07786173d49
dkMaster3000 <kunzd007@gmail.com>
2023-10-28 12:14:08 +0200

    yard working

--- cardgametut/Assets/Scripts/CardScripts/YardZoom.cs
+++ cardgametut/Assets/Scripts/CardScripts/YardZoom.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
+using UnityEngine.UI;
 
-public class YardZoom : NetworkBehaviour
+public class YardZoom : MonoBehaviour
 {
+
     public GameObject DisplayArea;
+    public GameObject ZoomCard;
+
     private GameObject zoomCard;
 
     void Start()
@@ -13,18 +16,16 @@ public class YardZoom : NetworkBehaviour
         DisplayArea = GameObject.Find("DisplayArea");
     }
 
-   public void OnHoverEnter()
+    public void OnHoverEnter()
     {
         if (transform.childCount != 0)
         {
             foreach (Transform child in transform)
             {
-                zoomCard = Instantiate(child.gameObject, new Vector2(0, 0), Quaternion.identity);
-                zoomCard.layer = LayerMask.NameToLayer("Zoom");
+                zoomCard = Instantiate(ZoomCard, new Vector2(0, 0), Quaternion.identity);
                 zoomCard.transform.SetParent(DisplayArea.transform, false);
 
-                RectTransform rect = zoomCard.GetComponent<RectTransform>();
-                rect.sizeDelta = new Vector2(240, 344);
+                zoomCard.GetComponent<Image>().sprite = child.GetComponent<Image>().sprite;
             }
         }
     }
